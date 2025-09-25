cf298cc34ed05370f9f4f818ea7fcec4ef74e865
EgorLodygin <52174254+EgorLodygin@users.noreply.github.com>
2025-09-26 01:17:03 +0500

    Add files via upload
    Первая лабараторная работа

---
+++ lab1.py
@@ -0,0 +1,52 @@
+'''print(123)
+print('123')
+print(1.23)
+
+print(1823 - 486)
+print(5.1 + 8.27)
+print(3 + 7.04 + 1 + 2.33)
+
+print('Привет, Мир')
+
+world = 'Мир'
+print(f"Привет, {world}!")
+
+one = 'Привет, '
+two = 'Мир!'
+print (one + two)
+
+one = 'Hello'
+print(bool(one))
+
+two = 142
+print(float(two))
+
+three = None
+print(str(three))
+
+one = input('one:')
+two = input('two:')
+three = input('three:')
+print(one, two, three)
+
+a = 12
+b = 5
+print('Возведение в степень:', a ** b)
+print('Обычное деление:', a / b)
+print('Целочисленное деление:', a // b)
+print('Нахождение остатка от деления:', a % b)
+
+line = 'Hello!'
+print(line * 6)
+
+sentence = 'Hello World!'
+print(sentence.count('o'))
+
+print('Hello,\nWorld!')
+
+sentence = 'Hello, World!'
+print(sentence[1])
+print(sentence[:5])'''
+
+
+
