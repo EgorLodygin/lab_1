aba6fd06b94a215cc0e5718d7226976d2a9743b5
EgorLodygin <52174254+EgorLodygin@users.noreply.github.com>
2025-09-26 01:59:39 +0500

    Add files via upload

---
+++ samRab1.py
@@ -0,0 +1,42 @@
+'''Задание 1'''
+one = False
+print(bool(one))
+
+'''Задание 2'''
+a = 'Хочу'
+b = 'сдать'
+c = 'работу'
+print(a, b, c)
+
+'''Задание 3'''
+print(1337 - 1330)
+
+'''Задание 4'''
+line = 'Hello'
+print(line * 4)
+
+'''Задание 5'''
+day, month, year = 25, "September", 2025
+print(f"Сегодня {day} {month} {year}.", end=" Всего хорошего!")
+
+'''Задание 6'''
+text = 'Hello World'
+print(text.replace(' ', ' my '))
+
+'''Задание 7'''
+sentence = 'Hello World'
+print(len(sentence))
+
+'''Задание 8'''
+sentence = 'Hello World'
+print(sentence.lower())
+
+'''Задание 9'''
+a = 1
+b = 2
+c = 3
+print(a + b * c)
+
+'''Задание 10'''
+sentence = 'Hello World'
+print(sentence.upper())
